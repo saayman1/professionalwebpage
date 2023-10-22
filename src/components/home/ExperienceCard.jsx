f79feaf6b6166ab43cb227d97d57c94e8acb73e0
saayman1 <147758496+saayman1@users.noreply.github.com>
2023-10-22 01:26:47 -0400

    Update ExperienceCard.jsx

--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-
-import {
-  Col,
-} from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 const ExperienceCard = ({ data }) => {
   return (
     <Col lg="6">
       <div className="pb-5 text-center">
-        <img className=" bg-white mb-3" src={data.companylogo} alt="" />
+        <img className=" bg-white mb-3" src={data.companylogo} alt={data.company} /> {/* Added the company name as alt text for the image */}
         <p className="lead">
           {data.role}
           <br />
+          {data.company} {/* This line displays the company name */}
+          <br />
           {data.date}
         </p>
-
       </div>
     </Col>
   );
