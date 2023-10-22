c1ad170a5301ca39678a5b79697c4ceb71edc8f7
saayman1 <147758496+saayman1@users.noreply.github.com>
2023-10-22 15:35:03 -0400

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -149,7 +149,7 @@ const experiences = {
     {
       role: 'Software Developer',
       company: 'Department of National Defence',
-      companylogo: require('../assets/img/Department-of-National-Defense-Government-of-Canada (1).jpeg'),
+      companylogo: require('../assets/img/Department-of-National-Defense-Government-of-Canada_image.jpeg'),
       date: 'May 2021 – March 2022',
     },
     {
