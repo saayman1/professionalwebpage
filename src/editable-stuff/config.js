ba47f2db7f29306be09a851194a742d60a9f981b
saayman1 <147758496+saayman1@users.noreply.github.com>
2023-10-22 22:07:55 -0400

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -149,7 +149,7 @@ const experiences = {
     {
       role: 'Software Developer',
       company: 'Department of National Defence',
-      companylogo: require('../assets/img/dnd_finale.jpg'),
+      companylogo: require('../assets/img/dnd_finale_final.jpg'),
       date: 'May 2021 – March 2022',
     },
     {
